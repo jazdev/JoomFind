14b6353f3a60ac1781aff0db8ce309c30d90b4cc
Jasdev Singh <jasdev@live.in>
2014-06-07 22:41:53 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='JoomFind',
+      version='0.1',
+      description='JoomFind - The Joomla CMS Detector',
+      author='Jasdev Singh',
+      author_email='jasdev@singh.am',
+      licence='MIT',
+      homepage='https://github.com/JoomFind',
+      description='Automated Joomla CMS detection',
+      scripts = ['JoomFind/JoomFind.py']
+     )
